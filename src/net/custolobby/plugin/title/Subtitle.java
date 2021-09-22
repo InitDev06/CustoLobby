387045791613148bbca1dc899f3b6ef28f8fb26d
AISimple <71404592+icodexs@users.noreply.github.com>
2021-09-22 14:49:08 -0300

    Delete src/net/custolobby/plugin/title directory

--- src/net/custolobby/plugin/title/Subtitle.java
+++
@@ -1,9 +0,0 @@
-package net.custolobby.plugin.title;
-
-import org.bukkit.entity.Player;
-
-public interface Subtitle {
-	
-	public void sendSubtitle(Player player, String message, int fadeIn, int stay, int fadeOut); 
-
-}
--- src/net/custolobby/plugin/title/Title.java
+++
@@ -1,9 +0,0 @@
-package net.custolobby.plugin.title;
-
-import org.bukkit.entity.Player;
-
-public interface Title {
-	
-	public void sendTitle(Player player, String message, int fadeIn, int stay, int fadeOut);
-
-}
